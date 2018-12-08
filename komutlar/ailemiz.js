c4bb15f181b3b7bd5e580d4bee7f75dea4e517ad
ismail0534 <45673957+ismail0534@users.noreply.github.com>
2018-12-09 00:19:07 +0300

    Update ailemiz.js

--- komutlar/ailemiz.js
+++ komutlar/ailemiz.js
@@ -24,6 +24,6 @@ exports.conf = {
 
 exports.help = {
   name: "ailemiz",
-  description: "Shows all the servers the bot is in.",
+  description: "Botun bulunduğu tüm sunucuları gösterir.",
   usage: "ailemiz"
 };
