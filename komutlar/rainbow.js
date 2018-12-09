fd4677cb60c973b71cad3ccba03e760fc1eca401
ismail0534 <45673957+ismail0534@users.noreply.github.com>
2018-12-09 19:33:09 +0300

    Delete rainbow.js

--- komutlar/rainbow.js
+++
@@ -1,9 +0,0 @@
-    client.on('message', async msg => {
-    if (msg.content.toLowerCase() === prefix + "rainbow") {
-   if (msg.channel.type === "dm") return;
-  const rol = 'Rainbow' // Rol ismi buraya
-  setInterval(() => {
-      msg.guild.roles.find(s => s.name === rol).setColor("RANDOM")
-      }, 9000);
-  }
-});
