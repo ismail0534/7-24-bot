bf5dbde00304c796eb274219c916db59674f25db
ismail0534 <45673957+ismail0534@users.noreply.github.com>
2019-01-12 16:16:30 +0300

    Add files via upload

---
+++ komutlar/söv.js
@@ -0,0 +1,64 @@
+exports.run = async (client, msg, args) => {
+  let kufur=[
+    "",
+    "amına kodumun jedayı seni",
+    "babanın annesinin amına koyayım orul orul orospu evladı",
+    "ebenin amı gibi kaşları da kara",
+	"ananın amına teletabinin antenlerini sokar göbeğindeki televizyondan ulusal porno yayını yaparımananı özgürlük heykelinin yanmayan meşalesinde siker şehri duman ederimhollywood bulvarında donla gezen ananın amına topuklu ayakkabı sokayımananı ikiz kulelerinin yedinci katına cıkartır amına uçakla girerim..ananın o dazlak kafasına teflon tavayla vurur sersemletir sikerim.ananın buruşmuş amına tefal ütü basar dümdüz ederim.ananın amına windows 7 kurar mavi ekran hatası verinceye kadar sikerim.ananın amına telefon kablosu sokar paralel hattan bacını skermgardolapta tangasını arayan ananın kafasını dolap kapagına sıkıştırır müjde ar gibi sikerimdağdan inmediği icin yüzme bilmeyen ananı büyük pasificte 1 ton boşalan beyaz balinalarla beraber-- siker olimpiyat yüzme şampiyonasında altın madalya kazandırırımkırmızı eşarp giyip ormanda kırmızı başlıklı kız gibi takılan anana kurt gibi yaklaşır amını param parça ederimmmpikachuuuuuuuuu diye camdan atlayan sipastik ananın amına poke topu fırlatırımananı balbasaurun sarmaşık kırbacıyla dolar pikacuyla kanka olur veririm elektriğiedisonla kanka olur ananı fakir mahallenizde yanmayan sokak direğine bağlar sike sike trafoyu patlatırımhani benim gençliğim nerde diyen orospu cocugu seni.tavşan kostümü giyip fotograf cektirince kendini playboy dergisinde kapak sanan ananın amına evdeki elektrik süpürgesini sokarımananla karşılıklı salsa yaparken piste takla atarak giren orospu cocugu kardeşini götünden sikerim..ananla karşılıklı sikişirken ay çekirdeği cıtlatırım kabuklarını babanın suratına fırlatırımevde göbeğini yere deydirerek sınav cekince kendini atletik sanan abini götünden sikeyim...saclarını arkaya tarayınca kendini tarık akan sanan babanıda götünden sikeyim...tokyo drifti izleyip köyde traktörle drift yapmaya calısan abinin götüne kamyonla gireyim..kilotlu corapla denize giren kız kardeşinin kafasını suya sokup bogulana kadar sikeyim..ananı çeeeeeeeeeeeeeeee ..googleye türbanlı karı sikişleri yazan dedeni götünden sikeyim.camiden tabut calıp dedesine tabut satan ananı tabutun icinde siker öldürürüm...ananın amına kolumu sokar kücük kardeşlerini cıkartırımananı neil amstrongla beraber aya cıkartıp siker hardcore movie alırım altın portakal film festivalinde aldıgım ödülü ananın amına sokarımananın amına harry poterin assasını sokar kücük kücük büyücüler cıkartırım..ananın amına pandora kutusu sokar icinden tavşan cıkartırımananın amına duracel pill atar 10 kata kadar daha güçlü sikerim.ananı national geographic belgeselinde sikerim insanlar arslan ciftlesmesi görür..ananın amına 5+1 hoparlör sokar kolonları titretirimananı hollandadaki altın portakal film festivaline götürür amına portakal ağacını sokarımanana gerilla kostümü giydirir şanlı türk ordusunun icine atıp harmandalı oynattıktan sonra amına c4 yerlestirip havaya ucururumananı ramsstein konserinde pistte sikerim du hast şarkısını tersten okuttururumbabanın o kokmuş corabını ananın amına sokarımananı galatasaray fenerbahçe derbisinde kale yapar musa sow gibi hatrick yaparımkaradenizin cılgın dalgalarında sörf yapmaya calısırqene diye fotograf cektiren bacının amına sörf tahtasını sokarımananın amına nokia 3310 sokar polifonik müzik eşliğinde sikerim.ananı klavyemin üstünde sikerken paintte yarak resmi cizip kız kardeşine gönderirim.ananı jerry kılıgına sokar tom gibi kovalarım elbet bir köşede yakalar sikerim.hain antuan diye haykıran kız kardeşini atımın üstünde yan giderken siker öldürürüm.ananı afrikada am kıtlığı yaşayan 3 bacaklı familyasının arasına atar paramparça olana kadar sikerim.ananı bruce lee gibi havada 30 parande atarak sikerimananı barnebau stadınnın orta sahasında sikerim 70 milyon ispanyol ollaaaaa diye bagırır ..ananla karşılıklı okey oynuyum okeyi ananın kafasına vurayım beyin trawmesi gecirriken sikerimananın amına polis jobu sokar ay memur bey lütfen yavaş olun diye çığlık attırırımananı hamit gibi 365 gün siker geriye kalan 6 saatte direğe vururumananı ağrı dağının eteğinde sikerim ibrahim tatlıses halay çekerananın amına kemençe sokar üzerinde horon teperimalt alta ben orospu cocuguyum yazınca kendini akrostiş şiir yazdım sanan orospu cocuguananla yamyam ayininde karşılıklı barış çubuğu içer totem heykelinin tepesine oturttururuminşaat direğinde striptiz yapmaya calısırken bacagını kıran ananın kafasına kiremit atar bayıltıp sikerim",
+    "seni anasının amında olimpiyat meşalesi yakıp 10 km kostuğumun cocuğu ",
+    "senin ananın amına mancınıkla patates atayım",
+    "senin dalağını öyle bi sikerim ki ömür boyu hıçkırırsın",
+    "ananın amına bilardo topuyla rövaşata çekerim am dolması yarrak kafalı çin orospusu seni amına kodumun çin aslanı bilyesini gibtiğimin am düdüğü",
+    "senin ananı düdüklü tencerenin arasına alıp öttüre öttüre sikerim ulan ben",
+    "ananın amını keserim cebime koyarım sıkıldıkça sikerim",
+    "ananın amına sınav yapar 2 milyon kişiyi sokarım",
+    "amına barut döker sürtünmeyle yakarım orospu evladı",
+    "seni bi sikerim bluetooh un hata verir kapsama alanın genişler.",
+    "o tuşlara basan ufacık parmaklarının arasına sokar felç edene kadar siker o felç olan parmaklarını kesip organ mafyasına satarım elde ettigim gelirin bi kısmını görme engelliler vakfına bağışlar kalan kısmıda annenle çarçur eder babanın delirmesini sağlar ocağına incir ağacı diker ağacın gölgesinde teyzeni dallı budaklı sikerim senin",
+    "küfür etmek günah olum sen ne yaptıysan artık sana kızmış birisi affettir kendini beni de günaha sokçak orospu kertenkelesi",
+    "küfür ederdim ama anan çok yordu",
+    "öyle yan durup şekilli mekilli tişört giyme ananı götünden siker Erol Taş gibi kiraz ağacından kamçı yapar döverim",
+  ]
+     let member = msg.mentions.members.first()
+   if(!member)return msg.channel.send({embed: {
+ color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+ description: (':no_entry_sign: Ya geçerli birini etiketle ya da sana sövmemi istiyosan kendini etiketle.')
+}});
+  if(member.id === "506369522094047242")return msg.channel.send({embed: {
+ color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+ description: (':no_entry_sign: Sen Kimsin Sahibime Sövüyon Amına Kodumun Jedayı.')
+}})
+  if(member.id === "4525389272581406738")return msg.channel.send({embed: {
+ color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+ description: (`:no_entry_sign: Hoop! Birşeyler ters gitti ! Kayn#0001 etiketlemişsin yarram!`)
+}})
+  if(member.id === "324641670132137985")return msg.channel.send({embed: {
+ color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+ description: (`:no_entry_sign: Hoop! Orda durucan Yapımcım ! Kayn#0001'na sövemezsin.`)
+}})
+  if(member.id === client.user.id){
+    msg.channel.send({embed: {
+ color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+ description: (`:no_entry_sign: Beni mi kandırcan orospu çocuğu ?`)
+}})
+  }
+  else{
+  msg.channel.send({embed: {
+ color: Math.floor(Math.random() * (0xFFFFFF + 1)),
+ description: (`${member} ${kufur[Math.floor(Math.random() * 16)]}.`)
+  }})
+  }
+  
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+ };
+ 
+exports.help = {
+  name: 'söv',
+  description: 'Birine Söver.',
+  usage: 'söv'
